337f47a464b1b4f8167d7f2301ed9bf12a55e12c
Haruki Sato <haruki-sugarsun@users.noreply.github.com>
2016-09-06 01:33:12 +0900

    Sync and Async examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,27 +8,41 @@ import org.apache.thrift.async.AsyncMethodCallback;
 
 public class Main {
     public static void main(String[] args) {
-        new ServerBuilder().port(8080, SessionProtocol.HTTP).serviceAt("/echo", THttpService.of(
-                new EchoService.AsyncIface() {
-                    @Override
-                    public void echo(String message, AsyncMethodCallback resultHandler) throws TException {
-                        resultHandler.onComplete(message);
-                    }
-                }, SerializationFormat.THRIFT_BINARY))
+        // Sync Server
+        new ServerBuilder().port(8080, SessionProtocol.HTTP)
+                .serviceAt("/echo", THttpService.of(
+                        new EchoService.Iface() {
+                            @Override
+                            public String echo(String message) throws TException {
+                                return message + " to sync-server";
+                            }
+                        }, SerializationFormat.THRIFT_BINARY))
+                .build().start();
+
+        // Async Server
+        new ServerBuilder().port(8081, SessionProtocol.HTTP)
+                .serviceAt("/echo", THttpService.of(
+                        new EchoService.AsyncIface() {
+                            @Override
+                            public void echo(String message, AsyncMethodCallback resultHandler) throws TException {
+                                resultHandler.onComplete(message + " to async-server");
+                            }
+                        }, SerializationFormat.THRIFT_BINARY))
                 .build().start();
 
         try {
-            // Sync
+            // Sync clients
             EchoService.Iface echoSyncClient = Clients.newClient(
                     "tbinary+h2c://localhost:8080/echo",
                     EchoService.Iface.class);
-            System.err.println(echoSyncClient.echo("Hello, Armeria Sync!"));
+            System.err.println(echoSyncClient.echo("Hello from sync-client"));
+            echoSyncClient = Clients.newClient(
+                    "tbinary+h2c://localhost:8081/echo",
+                    EchoService.Iface.class);
+            System.err.println(echoSyncClient.echo("Hello from sync-client"));
 
-            // Async
-            EchoService.AsyncIface echoAsyncClient = Clients.newClient(
-                    "tbinary+h2c://localhost:8080/echo",
-                    EchoService.AsyncIface.class);
-            echoAsyncClient.echo("Hello, Armeria Async!", new AsyncMethodCallback<String>() {
+            // Async clients
+            AsyncMethodCallback<String> handler = new AsyncMethodCallback<String>() {
                 @Override
                 public void onComplete(String response) {
                     System.err.println(response);
@@ -38,7 +52,15 @@ public class Main {
                 public void onError(Exception exception) {
                     exception.printStackTrace();
                 }
-            });
+            };
+            EchoService.AsyncIface echoAsyncClient = Clients.newClient(
+                    "tbinary+h2c://localhost:8080/echo",
+                    EchoService.AsyncIface.class);
+            echoAsyncClient.echo("Hello from async-client", handler);
+            echoAsyncClient = Clients.newClient(
+                    "tbinary+h2c://localhost:8081/echo",
+                    EchoService.AsyncIface.class);
+            echoAsyncClient.echo("Hello from async-client", handler);
         } catch (TException e) {
             e.printStackTrace();
         }
